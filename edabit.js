83d710a84b279332efcbaefa08ab831db39476dc
Laney Kawaguchi <lkmariko@gmail.com>
2020-02-08 15:55:45 -0700

    daily exercises

--- edabit.js
+++ edabit.js
@@ -1,3 +1,13 @@
 function addition(a, b) {
 	return a + b
 }
+
+function triArea(base, height) {
+	return (base * height) / 2
+}
+
+function lessThan100(a, b) {
+	if (a + b < 100)
+		return true
+	else return false
+}
