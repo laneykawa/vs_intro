7a342b1ef0c5baad31dd6bf836057d1b089ae30c
Laney Kawaguchi <lkmariko@gmail.com>
2020-02-12 19:45:32 -0700

    code challenge a day

--- edabit.js
+++ edabit.js
@@ -6,12 +6,21 @@ function triArea(base, height) {
 	return (base * height) / 2
 }
 
+// 2/09/2020 Warmup
+
 function lessThan100(a, b) {
 	if (a + b < 100)
 		return true
 	else return false
 }
 
+// 2/11/2020 Warmup, get first value
+
 function getFirstValue(arr) {
 	return arr[0];
+}
+
+// 2/12/2020
+function findPerimeter(height, width) {
+	return (2 * height) + (2 * width)	
 }
