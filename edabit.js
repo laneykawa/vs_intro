f5b03a35fb00535110e381b0c006446142c84864
Laney Kawaguchi <lkmariko@gmail.com>
2020-02-18 10:47:52 -0700

    daily exercise

--- edabit.js
+++ edabit.js
@@ -20,7 +20,22 @@ function getFirstValue(arr) {
 	return arr[0];
 }
 
-// 2/12/2020
+// 2/12/2020 Warmup
 function findPerimeter(height, width) {
 	return (2 * height) + (2 * width)	
+}
+
+function squared(b) {
+	return Math.pow(b,2);
+}
+
+function howManySeconds(hours) {
+	return hours * 60 * 60
+}
+
+//2/18/2020
+function lessThan100(a, b) {
+	if (a + b < 100)
+		return true
+	else return false
 }
