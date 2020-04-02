91f5798aad2c98c645a4526da451426b3fc6cf9e
Laney Kawaguchi <lkmariko@gmail.com>
2020-04-01 22:52:22 -0600

    daily exercise

--- edabit.js
+++ edabit.js
@@ -44,3 +44,15 @@ function lessThan100(a, b) {
 function isSameNum(num1, num2) {
 	return num1 === num2
 }
+
+// 3/5/2020 
+ function convert(hours, minutes) {
+	return (hours * 60 * 60) + (minutes * 60)
+}
+
+// 4/1/2020 Destructuring Arrays
+const arr = [1, 2, 3, 4, 5, 6]
+let a = arr[0]
+let b = arr[1]
+console.log(a)
+console.log(b)
